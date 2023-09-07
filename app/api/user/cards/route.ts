554d0789764203de6c35b7c7d49d74af302e8832
AndresDrimer <andresdrimer@hotmail.com>
2023-09-06 21:34:58 -0300

    functionality ok but slow performance

--- app/api/user/cards/route.ts
+++ app/api/user/cards/route.ts
@@ -9,19 +9,44 @@ export const PATCH = async (req: Request) => {
   console.log("cardId: ", cardId, "userId", userId)
     
 
+      const card = await prisma.card.findUnique({
+        where: {id: cardId},
+        include: {owners: true}
+      });
+
+    console.log("card", card)
+
+      const previousOwners = card?.owners.map(owner => owner.id) || []
+    console.log("prevOwners: ", previousOwners)
+if(previousOwners.includes(userId)){
+  //aca deberia en realidad borrarla, puede equivocarse al marcarla
+
+  const removeFromPreviousOwners = previousOwners.filter(it=> it!==userId)
+  const unOwnCard = await prisma.card.update({
+    where: {id: cardId},
+    data: {
+      ownersById: {
+        set: [...removeFromPreviousOwners]
+      }
+    }
+  })
+  return NextResponse.json({message: "Owner removed"}, {status: 201})
+}
 
     const updatedCard = await prisma.card.update({
       where: { id: cardId },
       data: {
-        owners: {
-          connect:userId.map((id: string) => ({ id })),
+        ownersById: {
+          set: [...previousOwners, userId],
         },
       },
     });
+
   console.log("updatedCard: ", updatedCard)
-    return NextResponse.json({ message: 'OK' }, { status: 201 });
+
+    return NextResponse.json({ message: 'Owner added' }, { status: 201 });
     } catch (error) {
-        return NextResponse.json({ message: 'Error' }, { status: 500 });
+        return NextResponse.json({ message: 'Connection Error' }, { status: 500 });
     }
   
   };
--- app/components/EachCard.tsx
+++ app/components/EachCard.tsx
@@ -1,34 +1,36 @@
 "use client"
 import { Card } from '@prisma/client'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation';
 
 
 function EachCard({it}: {it: Card}) {
+    const router = useRouter();
     const {data: session} = useSession();
-    const userId: string | undefined = session?.user.id
+    const userId: string  = session?.user.id ?? ""
 
+
+//Now it is working but it´s a littel annoying to have to wait to refresh page to change color, maybe it can be achived other way... useEffect??
 
 
     const selectCard = async (cardId: string) => { 
-      
-
-
-
       const res = await fetch(`/api/user/cards`, {
         method: "PATCH",
         headers: {
           "Content-type": "application/json"
         },
         body: JSON.stringify({cardId, userId})
-      })
+      });
+     // router.push("/dashboard") don´t know why this doesn´t work as it should be
+     window.location.href = "/dashboard"
+     };
 
-     }
-
-    
+    const owned = it.ownersById.includes(userId)
+console.log("owned", owned)
   return (
     <button
     key={it.absoluteNum}
-    className={`border-2 rounded-sm text-center hover:scale-105 text-xl py-2  ${it.isSelected ? "bg-green-500" : "bg-gray-900"}`}
+    className={`border-2 rounded-sm text-center hover:scale-105 text-xl py-2 ${owned ? "bg-emerald-500" : "bg-sky-950"}`}
     onClick={()=>selectCard(it.id)}
   ><p className='text-3xl'>{it.absoluteNum}</p>
    <p className='text-sm'>({it.team.substring(0, 3).toUpperCase()} - {it.teamNum})</p> 

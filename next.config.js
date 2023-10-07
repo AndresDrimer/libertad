d06efc7117e596e9e5a8b4ce6ecd8c6c4a65778d
AndresDrimer <andresdrimer@hotmail.com>
2023-10-07 04:48:41 -0300

    added serveraction nextconfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [],
-      },
-}
+  experimental:{
+      serverActions: true,
+  },
+};
 
 module.exports = nextConfig

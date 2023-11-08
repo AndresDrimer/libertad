2668e3b2be93a1cbb55570d480fcdc709f56c883
AndresDrimer <andresdrimer@hotmail.com>
2023-11-08 20:12:33 -0300

    base_url corrected

--- app/actions/server-actions.ts
+++ app/actions/server-actions.ts
@@ -8,7 +8,7 @@ import { revalidatePath } from "next/cache";
 export const selectCard = async (cardId: string, userId:string) => { 
 
   try {
-    const res = await fetch(`http://localhost:3000/api/user/cards`, {
+    const res = await fetch(`${process.env.BASE_URL}/api/user/cards`, {
       method: "PATCH",
       headers: {
         "Content-type": "application/json"
